562e93968bfa84e8eae46ee9a149e4bac9116ed5
faith5204 <48684127+faith5204@users.noreply.github.com>
2019-03-27 09:59:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+// set the correct username and password combination below
+var correctUsername = "faith5204";
+var correctPassword = "ilovemath24";
+
+
+$("button").click(function() {
+    var username = $(".username").val();
+    var password = $(".password").val();
+}  
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW THIS LINE
+if ("correctUsername" === "faith5204" && "correctPassword" === "ilovemath24") {
+     
+});
